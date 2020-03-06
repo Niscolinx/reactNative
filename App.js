e08ba30148ff17617fd2601cb47e38bfc4f14b0d
Niscolinx <munisco12@gmail.com>
2020-03-06 21:49:24 +0100

    update

--- App.js
+++ App.js
@@ -34,7 +34,6 @@ class App extends Component {
         <Text>Welcome to my first react native app</Text>
         <Text>Welcome to my first react native app</Text>
         <Text>Welcome to my first react native app</Text>
-        <Text>Welcome to my first react native app</Text>
   
       </View>
     );
